5ee274c087176dc9c7051d3c9f3fda97f4f73bf1
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-02-20 17:26:53 -0300

    js

--- src/app/http/middleware/log.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logMiddleware = (req, res, next) => {
-    console.log(`${new Date().toISOString()} - ${req.method} - ${req.originalUrl} - ${req.ip}`);
-    next();
-};
-exports.default = logMiddleware;

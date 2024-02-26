58c5ca05ba164102369b063ca899a5f13774cabf
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-02-26 16:48:09 -0300

    log

--- src/app/http/middleware/log.ts
+++ src/app/http/middleware/log.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 
 const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`${new Date().toISOString()} - ${req.method} - ${req.originalUrl} - ${req.ip}`);
+  var ip = req.socket.localAddress;
+  console.log(`${new Date().toISOString()} - ${req.method} - ${req.originalUrl} - ${ip} ${req.socket.remoteAddress}`);
   next();
 };
 
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+app.set('trust proxy', true);
+
 app.use(cors())
 
 //middlewares
